9617fc75811f1293f55945587f39e00c9e83b6f3
Lycaon <lycaon37@outlook.com>
2023-09-05 06:42:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* The Makings of a Programmer
+Console.WriteLine("Hello, World! I made you all cake!");
+Console.WriteLine("Woah woah woah.");
+Console.WriteLine("There's so many of you!");
+Console.WriteLine("I don't have enough cake for this...");
+Console.WriteLine("Listen guys... I'll come back with more soon. I promise."); */
+
+/* Consolas and Telim
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string RB = Console.ReadLine();
+Console.WriteLine("Noted: " + RB + " got bread."); */
+
+/* The Thing Namer 3000 
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // Noun
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // Adjective
+string c = "Doom"; // add "Doom"
+string d = "3000"; // add "3000".
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!"); // "of" is already included, so it was removed from "c". */
+
+/* The Variable Shop
+int Integer = 2;
+short ShortNumber = 5;
+long LongNumber = 200;
+byte ByteNumber = 25;
+sbyte SignedByte = -25;
+uint UIntNumber = 10;
+ushort UShortNumber = 11;
+ulong ULongNumber = 2000;
+char Character = 'a';
+string StringText = "Test String";
+float FloatNumber = 3.2f;
+double DoubleNumber = 3.2;
+decimal DecimalNumber = 3.2m;
+bool booltest = true;
+Console.WriteLine(Integer);
+Console.WriteLine(ShortNumber);
+Console.WriteLine(LongNumber);
+Console.WriteLine(ByteNumber);
+Console.WriteLine(SignedByte);
+Console.WriteLine(UIntNumber);
+Console.WriteLine(UShortNumber);
+Console.WriteLine(ULongNumber);
+Console.WriteLine(Character);
+Console.WriteLine(StringText);
+Console.WriteLine(FloatNumber);
+Console.WriteLine(DoubleNumber);
+Console.WriteLine(DecimalNumber);
+Console.WriteLine(booltest); */
+
+/* The Variable Shop Returns
+Console.WriteLine("Begin Original Variables");
+int Integer = 2;
+short ShortNumber = 5;
+long LongNumber = 200;
+byte ByteNumber = 25;
+sbyte SignedByte = -25;
+uint UIntNumber = 10;
+ushort UShortNumber = 11;
+ulong ULongNumber = 2000;
+char Character = 'a';
+string StringText = "Test String";
+float FloatNumber = 3.2f;
+double DoubleNumber = 3.2;
+decimal DecimalNumber = 3.2m;
+bool booltest = true;
+Console.WriteLine(Integer);
+Console.WriteLine(ShortNumber);
+Console.WriteLine(LongNumber);
+Console.WriteLine(ByteNumber);
+Console.WriteLine(SignedByte);
+Console.WriteLine(UIntNumber);
+Console.WriteLine(UShortNumber);
+Console.WriteLine(ULongNumber);
+Console.WriteLine(Character);
+Console.WriteLine(StringText);
+Console.WriteLine(FloatNumber);
+Console.WriteLine(DoubleNumber);
+Console.WriteLine(DecimalNumber);
+Console.WriteLine(booltest);
+Console.WriteLine("Begin New Variables");
+Integer = 4;
+ShortNumber = 3;
+LongNumber = 250;
+ByteNumber = 20;
+SignedByte = -55;
+UIntNumber = 20;
+UShortNumber = 12;
+ULongNumber = 20000;
+Character = 'c';
+StringText = "New Test String";
+FloatNumber = 3.4f;
+DoubleNumber = 3.8;
+DecimalNumber = 3.5m;
+booltest = false;
+Console.WriteLine(Integer);
+Console.WriteLine(ShortNumber);
+Console.WriteLine(LongNumber);
+Console.WriteLine(ByteNumber);
+Console.WriteLine(SignedByte);
+Console.WriteLine(UIntNumber);
+Console.WriteLine(UShortNumber);
+Console.WriteLine(ULongNumber);
+Console.WriteLine(Character);
+Console.WriteLine(StringText);
+Console.WriteLine(FloatNumber);
+Console.WriteLine(DoubleNumber);
+Console.WriteLine(DecimalNumber);
+Console.WriteLine(booltest); */
+
+/* The Triangle Farmer */
