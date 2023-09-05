422c38897661004ee12bcb449014a3c7ea6358b6
Lycaon <lycaon37@outlook.com>
2023-09-05 07:42:29 -0500

    feat: Add more solutions

---
+++ Solutions/TheDominionOfKings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProgram.Solutions
+{
+    public partial class Solution
+    {
+        public static void TheDominionOfKings()
+        {
+            // The Dominion of Kings
+            Console.WriteLine("For you, how many estates do you have?");
+            int Estates = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now, how many duchies?");
+            int Duchies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("And provinces?");
+            int Provinces = Convert.ToInt32(Console.ReadLine());
+            int Total = Estates + (Duchies * 3) + (Provinces * 6);
+            Console.WriteLine($"With {Estates} estates, {Duchies} duchies, and {Provinces} provinces, you have a total of {Total} points.");
+        }
+    }
+}
---
+++ Solutions/TheFourSistersAndTheDuckbear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProgram.Solutions
+{
+    public partial class Solution
+    {
+        public static void TheFourSistersAndTheDuckbear()
+        {
+            // The Four Sisters and the Duckbear
+            Console.WriteLine("How many eggs were collected?");
+            int Eggs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many sisters?");
+            int Sisters = Convert.ToInt32(Console.ReadLine());
+            int EggsPerSister = Eggs / Sisters;
+            int EggsForDuckbear = Eggs % Sisters;
+            Console.WriteLine($"Each sister gets {EggsPerSister}. The duckbear gets {EggsForDuckbear}.");
+            // Values where the duckbear gets more than the sisters: 1,2,3 (0 per sister).
+        }
+    }
+}
